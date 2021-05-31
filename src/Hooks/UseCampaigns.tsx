0f29fb128b214d893c77f56b33de05ec1281512e
CodingVampyre <tobiaskavsek@hotmail.de>
2021-05-31 21:22:57 +0200

    :wrench: add hook based on lodash

---
+++ src/Hooks/UseCampaigns.tsx
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import {ICampaign} from "../Domain/ICampaign";
+import * as _ from 'lodash';
+import {createEmptyCharacter} from "../Objects/createEmptyCharacter";
+
+export function useCampaigns() {
+
+	const [campaigns, setCampaigns] = useState<ICampaign[]>([]);
+	const [currentCampaign, setCurrentCampaign] = useState<number | undefined>(undefined);
+
+	// CAMPAIGN MANAGEMENT
+
+	function createCampaign(name: string): void {
+		setCampaigns(last => {
+			const campaigns = _.cloneDeep(last);
+			campaigns.push({ characterDrafts: [], characters: [], name });
+			return campaigns;
+		});
+	}
+
+	function setCampaign(index: number) {
+		setCurrentCampaign(index);
+	}
+
+	// CHARACTERS
+
+	function addCharacterDraft(characterName: string): number {
+		let id;
+		setCampaigns(last => {
+			const campaigns = _.cloneDeep(last);
+			const campaign = campaigns[currentCampaign];
+			id = campaign.characterDrafts.push(createEmptyCharacter(characterName, campaign.name));
+			return campaigns;
+		});
+		return id;
+	}
+
+	return {
+		campaigns,
+		currentCampaign,
+		createCampaign,
+		setCampaign,
+	};
+}
